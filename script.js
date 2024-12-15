fe5c12e0c7681266bb377e3f9fe97f9394657628
K. Sudhir <151105165+k-sudhir-1@users.noreply.github.com>
2024-12-15 00:12:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fetch data from the JSON file
 fetch('papers.json')
     .then(response => response.json())
     .then(data => {
@@ -6,6 +7,7 @@ fetch('papers.json')
         renderPapers(papers);
     });
 
+// Setup keyword buttons
 function setupKeywords(keywords) {
     ['journal', 'topic', 'method'].forEach(group => {
         const container = document.getElementById(`${group}-keywords`);
@@ -18,6 +20,7 @@ function setupKeywords(keywords) {
     });
 }
 
+// Render filtered papers
 function renderPapers(filteredPapers) {
     const list = document.getElementById('papers-list');
     list.innerHTML = '';
@@ -28,6 +31,7 @@ function renderPapers(filteredPapers) {
     });
 }
 
+// Filter papers based on keyword selection
 function filterPapers(group, keyword) {
     fetch('papers.json')
         .then(response => response.json())
